918dfc3fbdde82e5b0b6e5d3250bba5fd2b7fbbb
Arthur Kroth Posselt <akposselt@gmail.com>
2026-04-07 21:01:40 -0300

    feat: add ToDoController for managing ToDo operations

---
+++ src/main/java/com/to_do_list/controller/ToDoController.java
@@ -0,0 +1,45 @@
+package com.to_do_list.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.to_do_list.entity.ToDo;
+import com.to_do_list.service.ToDoService;
+
+@RestController
+@RequestMapping("/todos")
+public class ToDoController {
+    
+    private ToDoService toDoService;
+
+    public ToDoController(ToDoService toDoService) {
+        this.toDoService = toDoService;
+    }
+
+    @PostMapping
+    List<ToDo> create(@RequestBody ToDo toDo) {
+        return toDoService.create(toDo);
+    }
+
+    @GetMapping
+    List<ToDo> list() {
+        return toDoService.list();
+    }
+
+    @PutMapping
+    List<ToDo> update(@RequestBody ToDo toDo) {
+        return toDoService.update(toDo);
+    }
+
+    @DeleteMapping("{id}")
+    List<ToDo> delete(@PathVariable("id") Long id) {
+        return toDoService.delete(id);
+    }
+}
