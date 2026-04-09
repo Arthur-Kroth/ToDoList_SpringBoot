d8e9b952ad5141cbcb7d47bb88baa2fc94490e65
Arthur Kroth Posselt <akposselt@gmail.com>
2026-04-08 23:47:22 -0300

    feat: @NotBlank validation in name and description on ToDo entity

--- src/main/java/com/to_do_list/entity/ToDo.java
+++ src/main/java/com/to_do_list/entity/ToDo.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "to_do")
@@ -13,7 +14,9 @@ public class ToDo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank
     private String name;
+    @NotBlank
     private String description;
     private boolean completed;
     private int priority;
